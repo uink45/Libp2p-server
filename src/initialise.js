4b30d7771cdec4bbf16ddd7a29763cb972d6685d
uink45 <79078981+uink45@users.noreply.github.com>
2022-03-21 22:06:54 +1000

    Use the older method for fetching state.

--- src/initialise.js
+++ src/initialise.js
@@ -1,5 +1,5 @@
 const types = require("@chainsafe/lodestar-types");
-const { fetchWeakSubjectivityState } = require("@chainsafe/lodestar-cli/lib/networks");
+const { fetchWeakSubjectivityState } = require("./network/networks/index");
 const { getBeaconConfigFromArgs } = require("@chainsafe/lodestar-cli/lib/config/beaconParams")
 const { args } = require("./args");
 
@@ -9,8 +9,11 @@ const { args } = require("./args");
 async function fetchState(){
     const config = getBeaconConfigFromArgs(args);
     const remoteBeaconUrl = "https://21qajKWbOdMuXWCCPEbxW1bVPrp:5e43bc9d09711d4f34b55077cdb3380a@eth2-beacon-mainnet.infura.io";
-    const state = await fetchWeakSubjectivityState(config, {weakSubjectivityServerUrl: remoteBeaconUrl});
-    const stateTreeBacked = types.ssz.altair.BeaconState.createTreeBackedFromStruct(state.wsState);
+    const stateId = "head";
+    const url = `${remoteBeaconUrl}/eth/v1/debug/beacon/states/${stateId}`;
+    const state = await fetchWeakSubjectivityState(config, url);
+    const stateTreeBacked = types.ssz.altair.BeaconState.createTreeBackedFromStruct(state);
+
     return stateTreeBacked;
 }
 exports.fetchState = fetchState;
